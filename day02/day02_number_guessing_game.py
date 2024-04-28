44b0d72679ffa97373c88a5de7d4728cb29f93aa
Mazalik <166901653+Mazalik@users.noreply.github.com>
2024-04-28 23:51:16 +0300

    Add files via upload

---
+++ day02/day02_number_guessing_game.py
@@ -0,0 +1,16 @@
+import random
+
+number = random.randrange(1,21)
+rounds = 0
+
+while True:
+    rounds += 1
+    user_guess = int(input("Guess a number between 1 and 20 "))
+    if user_guess == number:
+        print(f"Good guess!")
+        break
+    elif user_guess < number:
+        print(f"your guess is too small, try again \n")
+    elif user_guess > number:
+        print(f"your guess is too big, try again")
+print(f"You found the correct number after {rounds} rounds")
