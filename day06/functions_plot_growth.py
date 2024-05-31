75ba2b0fe960fd2c869fb3caf1d1c87d7e565a96
Mazalik <166901653+Mazalik@users.noreply.github.com>
2024-05-31 17:05:23 +0300

    n

---
+++ day06/functions_plot_growth.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def read_data(x):
+    turbidity_data = pd.read_excel(x)
+    
+    if not any(turbidity_data.columns == ['time_hr','raw_turbidity_OD600','dilution_factor']):
+        print("make sure your file is in the correct tamplate")
+        return False
+    else:
+        return turbidity_data
+
+def calculate_real_turbidity(turbidity_data):
+    turbidity_data['turbidity_OD600'] = turbidity_data.raw_turbidity_OD600 * turbidity_data.dilution_factor
+    return turbidity_data
+
--- day06/plot_growth.py
+++ day06/plot_growth.py
@@ -1,13 +1,28 @@
 import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
+import functions_plot_growth
 
-growth_data = pd.read_excel('growth_data.xlsx')
-data_titles = growth_data.columns
+def main():
+    x = input("Please enter the full name of the excel file you would like to work on")
 
-growth_data.plot.scatter(x='Time (hr)',y='Turbidity (OD600)')
-plt.savefig('growth_plot.png')
-plt.show()
+    turbidity_data = functions_plot_growth.read_data(x)
+    print(turbidity_data)
+    #if not turbidity_data:
+    #    exit
+    
+    ##turbidity_data = functions_plot_growth.calculate_real_turbidity(turbidity_data)
+    
+    #turbidity_data['turbidity_OD600'] = turbidity_data.raw_turbidity_OD600 * turbidity_data.dilution_factor
+
+    ##turbidity_data.plot.scatter(x='time_hr', y='turbidity_OD600')
+    ##plt.savefig('growth_plot.png')
+    ##plt.show()
+
+if __name__ == "__main__":
+    main()
+
+
 
 
 
